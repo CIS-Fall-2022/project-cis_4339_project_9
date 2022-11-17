41f4cc2843de5bd1a1c01f97f9834af46cf4a4ba
Ryan Delgado <70501188+RyanDelgado@users.noreply.github.com>
2022-11-16 19:21:49 -0600

    Master Upload for Cloud Deployment

---
+++ frontend2/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+// eslint-disable-next-line no-undef
+const path = require("path");
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 8001,
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      // eslint-disable-next-line no-undef
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
