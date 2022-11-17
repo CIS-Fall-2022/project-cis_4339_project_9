36c75c80bc14dfcc8998ccafd101b6d351a1281d
Ryan Delgado <70501188+RyanDelgado@users.noreply.github.com>
2022-11-16 19:22:54 -0600

    Add files via upload

---
+++ frontend3/src/components/EventChart.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <!-- canvas element for chart -->
+    <canvas id="event-chart"></canvas>
+  </div>
+</template>
+
+
+<script>
+import { Chart, registerables } from 'chart.js'
+import eventChartData from '../assets/event-data.js'
+
+//we have to register the registerables with Chart object
+Chart.register(...registerables);
+export default {
+  name: 'EventChart',
+  data() {
+    return {
+      eventChartData: eventChartData
+    }
+  },
+  //establish Chart object after mounting the component
+  mounted() {
+    const ctx = document.getElementById('event-chart');
+    new Chart(ctx, this.eventChartData);
+  }
+}
+</script>
